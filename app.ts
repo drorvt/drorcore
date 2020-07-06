eeb0159f59b0ef95367b13dff91b16886351211a
Yonatan Back <yonatanback@gmail.com>
2020-07-06 15:47:17 +0300

    Added dotenv to app make sure to add .env file to app root dir

--- app.ts
+++ app.ts
@@ -1,10 +1,15 @@
 import express = require("express");
- 
+require('dotenv').config();
+console.log(process.env.SHOPIFY_SHOP_NAME);
+import { shopifyRouter } from "./src/routes/shopify.router";
+
 const app: express.Application = express();
  
  app.get("/", function (req, res) {
      res.send("Hello World!");
 });
+app.use("/shopify", shopifyRouter);
+
 
  app.listen(3000, function () {
      console.log("App is listening on port 3000!");
