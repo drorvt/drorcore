29ce0ac9ea832755e7aa9c7f78a3bbf0a596bd9a
Yonatan Back <yonatanback@gmail.com>
2020-07-08 23:02:11 +0300

    added shopify routes

--- src/routes/shopify.router.ts
+++ src/routes/shopify.router.ts
@@ -2,8 +2,9 @@
  * Required External Modules and Interfaces
  */
 import express, { Request, Response } from "express";
-import * as ShopifyService from '../services/shopify.service';
+import * as ShopifyService from "../services/shopify.service";
 import { Product } from "../models/Product";
+import { logger } from "../utils/logger";
 
 /**
  * Router Definition
@@ -17,19 +18,53 @@ export const shopifyRouter = express.Router();
 // GET items/
 
 shopifyRouter.get("/getAllProducts", async (req: Request, res: Response) => {
+  try {
+    const products: Product[] = await ShopifyService.getAllProducts();
+    res.status(200).send(products);
+  } catch (e) {
+    res.status(404).send(e.message);
+  }
+});
+
+//get product by ID:
+shopifyRouter.get(
+  "/products/:productId",
+  async (req: Request, res: Response) => {
     try {
-      const products: Product[] = await ShopifyService.findAll();
-  
-      res.status(200).send(products);
+      //+ parses string to number
+      const product: Product = await ShopifyService.findProduct(
+        +req.params.productId
+      );
+      res.status(200).send(product);
     } catch (e) {
       res.status(404).send(e.message);
     }
-  });
+  }
+);
 
-  
-  
+shopifyRouter.get("/getAllTags", async (req: Request, res: Response) => {
+  try {
+    const tags: String[] = await ShopifyService.getAllTags();
+    res.status(200).send(tags);
+  } catch (e) {
+    res.status(404).send(e.message);
+  }
+});
+
+//get all tags for product
+shopifyRouter.get("/tags/:productId", async (req: Request, res: Response) => {
+  try {
+    const tags: String[] = await ShopifyService.getProductTags(
+      +req.params.productId
+    );
+    res.status(200).send(tags);
+  } catch (e) {
+    res.status(404).send(e.message);
+  }
+});
+
 //   // POST items/
-  
+
 //   // PUT items/
-  
+
 //   // DELETE items/:id
