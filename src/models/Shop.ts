e616f4a7ad38cf5d42d9dceb6dcc8273c47d8c5a
Yonatan Back <yonatanback@gmail.com>
2020-07-09 12:42:29 +0300

    tslint + prettier

--- src/models/Shop.ts
+++ src/models/Shop.ts
@@ -1,33 +1,33 @@
-import { Column, Entity, ManyToMany, PrimaryGeneratedColumn } from "typeorm";
-import { User } from "./User";
+import { Column, Entity, ManyToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { User } from './User';
 
-@Entity("shops", { schema: "pdq" })
+@Entity('shops', { schema: 'pdq' })
 export class Shop {
-  @PrimaryGeneratedColumn({ type: "int", name: "id" })
-  id: number;
+    @PrimaryGeneratedColumn({ type: 'int', name: 'id' })
+    id: number;
 
-  @Column("varchar", { name: "name", nullable: true, length: 100 })
-  name: string | null;
+    @Column('varchar', { name: 'name', nullable: true, length: 100 })
+    name: string | null;
 
-  @Column("varchar", { name: "url", nullable: true, length: 100 })
-  url: string | null;
+    @Column('varchar', { name: 'url', nullable: true, length: 100 })
+    url: string | null;
 
-  @Column("timestamp", {
-    name: "created",
-    nullable: true,
-    default: () => "CURRENT_TIMESTAMP",
-  })
-  created: Date | null;
+    @Column('timestamp', {
+        name: 'created',
+        nullable: true,
+        default: () => 'CURRENT_TIMESTAMP'
+    })
+    created: Date | null;
 
-  @Column("varchar", { name: "api_key", nullable: true, length: 100 })
-  apiKey: string | null;
+    @Column('varchar', { name: 'api_key', nullable: true, length: 100 })
+    apiKey: string | null;
 
-  @Column("varchar", { name: "secret_key", nullable: true, length: 100 })
-  secretKey: string | null;
+    @Column('varchar', { name: 'secret_key', nullable: true, length: 100 })
+    secretKey: string | null;
 
-  @Column("int", { name: "owner_id", nullable: true })
-  ownerId: number | null;
+    @Column('int', { name: 'owner_id', nullable: true })
+    ownerId: number | null;
 
-  @ManyToMany(type => User, user => user.shops)
-  users: User[];
+    @ManyToMany(type => User, user => user.shops)
+    users: User[];
 }
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,46 +1,47 @@
 import {
-  Column,
-  Entity,
-  Index,
-  JoinColumn,
-  ManyToOne,ManyToMany, JoinTable,
-  PrimaryGeneratedColumn,
-} from "typeorm";
-import { Shop } from "./Shop";
+    Column,
+    Entity,
+    Index,
+    JoinColumn,
+    ManyToOne,
+    ManyToMany,
+    JoinTable,
+    PrimaryGeneratedColumn
+} from 'typeorm';
+import { Shop } from './Shop';
 
-@Index("users_FK", ["shopId"], {})
-@Entity("users", { schema: "pdq" })
-export class User  {
+@Index('users_FK', ['shopId'], {})
+@Entity('users', { schema: 'pdq' })
+export class User {
+    @Column('varchar', { name: 'password', nullable: true, length: 100 })
+    password: string | null;
 
-  @Column("varchar", { name: "password", nullable: true, length: 100 })
-  password: string | null;
+    @Column('varchar', { name: 'email', nullable: true, length: 100 })
+    email: string | null;
 
-  @Column("varchar", { name: "email", nullable: true, length: 100 })
-  email: string | null;
+    @PrimaryGeneratedColumn({ type: 'int', name: 'id' })
+    id: number;
 
-  @PrimaryGeneratedColumn({ type: "int", name: "id" })
-  id: number;
+    @Column('int', { name: 'shop_id', nullable: true })
+    shopId: number | null;
 
-  @Column("int", { name: "shop_id", nullable: true })
-  shopId: number | null;
+    @Column('tinyint', { name: 'is_view', nullable: true, width: 1 })
+    isView: boolean | null;
 
-  @Column("tinyint", { name: "is_view", nullable: true, width: 1 })
-  isView: boolean | null;
+    @Column('tinyint', { name: 'is_write', nullable: true, width: 1 })
+    isWrite: boolean | null;
 
-  @Column("tinyint", { name: "is_write", nullable: true, width: 1 })
-  isWrite: boolean | null;
+    @Column('tinyint', { name: 'is_admin', nullable: true, width: 1 })
+    isAdmin: boolean | null;
 
-  @Column("tinyint", { name: "is_admin", nullable: true, width: 1 })
-  isAdmin: boolean | null;
+    @Column('timestamp', {
+        name: 'created',
+        nullable: true,
+        default: () => 'CURRENT_TIMESTAMP'
+    })
+    created: Date | null;
 
-  @Column("timestamp", {
-    name: "created",
-    nullable: true,
-    default: () => "CURRENT_TIMESTAMP",
-  })
-  created: Date | null;
-
-  @ManyToMany(type => Shop, shop => shop.users)
-  @JoinTable()
-  shops: Shop[];
+    @ManyToMany(type => Shop, shop => shop.users)
+    @JoinTable()
+    shops: Shop[];
 }
