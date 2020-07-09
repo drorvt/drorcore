93f9cfd1c968db279a1746e5e23a036daa659b20
ShlomoSchwarcz <shlomo@j2ee.co.il>
2020-07-09 11:43:56 +0300

    allow all functions for admin role

--- src/services/authorization.service.ts
+++ src/services/authorization.service.ts
@@ -1,7 +1,12 @@
 export default function authorize(...allowed: string[]) {
     return (request: any, response: any, next: () => any) => {
         let roles = request?.user?.roles;
-        let isAllowed: boolean = allowed.includes('public') || allowed.some(role => roles.includes(role));
+        let isAllowed: boolean = false;
+        isAllowed = allowed.includes('public') || allowed.some(role => roles.includes(role));
+        if (roles.includes('admin')){
+            isAllowed = true;
+        }
+
         if (isAllowed)
             next();
         else {
