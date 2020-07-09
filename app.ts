c6e587038980e3f9ce581ac648e194433f2be7ae
Yonatan Back <yonatanback@gmail.com>
2020-07-09 11:36:54 +0300

    added shopify router tslinted

--- app.ts
+++ app.ts
@@ -1,60 +1,61 @@
-import express = require("express");
-import {ensureAuthenticated} from "./src/services/authentication.service";
-import {createConnection} from "typeorm";
+import express = require('express');
+import { ensureAuthenticated } from './src/services/authentication.service';
+import { createConnection } from 'typeorm';
 import path = require('path');
 import passport = require('passport');
 const bodyParser = require('body-parser');
 const session = require('express-session');
-import {logger} from "./src/utils/logger";
-import {initDB, buildDemoDB} from './tests/my-test';
-
+import { logger } from './src/utils/logger';
+import { initDB, buildDemoDB } from './tests/my-test';
 
 require('dotenv').config();
 console.log(process.env.SHOPIFY_SHOP_NAME);
-const production:any = process.env.PRODOCTION;
+const production: any = process.env.PRODOCTION;
 console.log('Production: ' + process.env.PRODOCTION);
 
-import { shopifyRouter } from "./src/routes/shopify.router";
+import { shopifyRouter } from './src/routes/shopify.router';
+import { userRouter } from './src/routes/user.router';
 
 const app: express.Application = express();
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(session({
-    secret: 'secrettexthere',
-    saveUninitialized: true,
-    resave: true,
-  }));
+app.use(
+    session({
+        secret: 'secrettexthere',
+        saveUninitialized: true,
+        resave: true
+    })
+);
 app.use(passport.initialize());
 app.use(passport.session());
-// app.use("/shopify", shopifyRouter);
-import { userRouter } from "./src/routes/user.router";
+
 app.use('/', userRouter);
+app.use('/shopify', shopifyRouter);
 app.all('/*', ensureAuthenticated);
 
-app.get("/", function (req, res) {
-    res.send("Hello World!");
+app.get('/', function (req, res) {
+    res.send('Hello World!');
 });
 
 app.use(express.static(path.join(__dirname, '/public')));
 
 const startServer = async () => {
     // Initialize test Database
-    if (!production){
+    if (!production) {
         await initDB();
     }
 
-    await createConnection().then((con) => {
+    await createConnection().then(con => {
         logger.info('info', 'created Database connection');
     });
-    
+
     await buildDemoDB();
-    
+
     app.listen(3000, function () {
         logger.info('info', 'App is listening on port 3000!');
     });
-}
+};
 
 startServer();
-
