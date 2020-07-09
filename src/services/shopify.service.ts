a056f5c9a3c6e3b79a3b80c7217aa7ab9d0e450e
Yonatan Back <yonatanback@gmail.com>
2020-07-09 11:47:38 +0300

    tslint + prettier

--- src/services/shopify.service.ts
+++ src/services/shopify.service.ts
@@ -1,5 +1,5 @@
-import Shopify from "shopify-api-node";
-import { Product } from "../models/Product";
+import Shopify from 'shopify-api-node';
+import { Product } from '../models/Product';
 
 /**
  * In-Memory Store
@@ -8,9 +8,9 @@ import { Product } from "../models/Product";
 const products: Product[] = [{ id: 1 }, { id: 2 }];
 // const shops = {};
 const shopify = new Shopify({
-  shopName: process.env.SHOPIFY_SHOP_NAME || "",
-  apiKey: process.env.SHOPIFY_API_KEY || "",
-  password: process.env.SHOPIFY_API_PASSWORD || "",
+    shopName: process.env.SHOPIFY_SHOP_NAME || '',
+    apiKey: process.env.SHOPIFY_API_KEY || '',
+    password: process.env.SHOPIFY_API_PASSWORD || ''
 });
 
 /**
@@ -18,40 +18,40 @@ const shopify = new Shopify({
  */
 
 export const getAllProducts = async (): Promise<Product[]> => {
-  return shopify.product.list({ limit: 250 });
+    return shopify.product.list({ limit: 250 });
 };
 
 export const findProduct = async (productId: number): Promise<Product> => {
-  try {
-    return shopify.product.get(productId);
-  } catch (e) {
-    throw new Error("No record found");
-  }
+    try {
+        return shopify.product.get(productId);
+    } catch (e) {
+        throw new Error('No record found');
+    }
 };
 
 export const getProductTags = (productId: number): Promise<String[]> => {
-  try {
-    // split and trim tags
-    return shopify.product
-      .get(productId)
-      .then((x) => x.tags.split(",").map((i) => i.trim()));
-  } catch (e) {
-    throw new Error("Error getting tags for product " + productId);
-  }
+    try {
+        // split and trim tags
+        return shopify.product
+            .get(productId)
+            .then(x => x.tags.split(',').map(i => i.trim()));
+    } catch (e) {
+        throw new Error('Error getting tags for product ' + productId);
+    }
 };
 
 export const getAllTags = async (): Promise<String[]> => {
-  try {
-    // get all tags, split, trim, and flatten
-    const tags = (await shopify.product.list())
-      .map((x) => x.tags.split(",").map((x) => x.trim()))
-      .reduce((x, y) => x.concat(y), []);
-      
-    // return unique values:
-    return tags.filter(function (item, pos, self) {
-      return self.indexOf(item) == pos;
-    });
-  } catch (e) {
-    throw new Error("Error getting tags");
-  }
+    try {
+        // get all tags, split, trim, and flatten
+        const tags = (await shopify.product.list())
+            .map(x => x.tags.split(',').map(x => x.trim()))
+            .reduce((x, y) => x.concat(y), []);
+
+        // return unique values:
+        return tags.filter(function (item, pos, self) {
+            return self.indexOf(item) == pos;
+        });
+    } catch (e) {
+        throw new Error('Error getting tags');
+    }
 };
