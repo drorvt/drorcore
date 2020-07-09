33976b8bbeb7df62c2a771d86d6201aed22b3340
Yonatan Back <yonatanback@gmail.com>
2020-07-09 11:47:00 +0300

    tslint + prettier

--- src/models/LoggedInUser.ts
+++ src/models/LoggedInUser.ts
@@ -1,11 +1,11 @@
-export class LoggedInUser{
-    userName:string|null;
-    id:number;
-    store:number|undefined;
+export class LoggedInUser {
+    userName: string | null;
+    id: number;
+    store: number | undefined;
     loggedSince: Date;
-    roles: Array<string> = ["read"]
+    roles: Array<string> = ['read'];
 
-    constructor(userName:string|null, id:number){
+    constructor(userName: string | null, id: number) {
         this.userName = userName;
         this.id = id;
         this.loggedSince = new Date();
