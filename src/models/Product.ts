423a008e026406a7fa82e43eec71353a321df938
Yonatan Back <yonatanback@gmail.com>
2020-07-09 15:15:34 +0300

    Added productTag Changed Product to use typeORM

--- src/models/Product.ts
+++ src/models/Product.ts
@@ -1,3 +1,30 @@
+import {
+    Column,
+    Entity,
+    ManyToMany,
+    PrimaryGeneratedColumn,
+    JoinTable
+} from 'typeorm';
+import { ProductTag } from './ProductTag';
+
+@Entity('products', { schema: 'pdq' })
 export class Product {
+    @PrimaryGeneratedColumn()
     id: number;
+
+    @Column('int')
+    shopId: number | null;
+
+    @Column('varchar')
+    name: string | null;
+
+    @Column('varchar')
+    productType: string | null;
+
+    @Column('timestamp', { default: () => 'CURRENT_TIMESTAMP' })
+    updated: Date | null;
+
+    @ManyToMany(type => ProductTag, productTag => productTag.id)
+    @JoinTable()
+    productTags: ProductTag[] | null;
 }
--- src/models/ProductTag.ts
+++ src/models/ProductTag.ts
@@ -1,10 +1,18 @@
 import { Column, Entity, ManyToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { Product } from './Product';
 
 @Entity()
 export class ProductTag {
+    constructor(name: string) {
+        this.name = name;
+    }
+
     @PrimaryGeneratedColumn()
     id: number;
 
-    @Column()
+    @Column('string')
     name: string | null;
+
+    @ManyToMany(type => Product, product => product.productTags)
+    products: Product[] | null;
 }
