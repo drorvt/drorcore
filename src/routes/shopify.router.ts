cab457261c80658d2ef2cf1dd2dfb313c9ff888a
Yonatan Back <yonatanback@gmail.com>
2020-07-13 20:15:19 +0300

    tslint + Prettier Changed some method names, routes

--- src/routes/shopify.router.ts
+++ src/routes/shopify.router.ts
@@ -1,10 +1,16 @@
 /**
  * Required External Modules and Interfaces
  */
-import express, { Request, Response } from "express";
-import * as ShopifyService from "../services/shopify.service";
-import { Product } from "../models/Product";
-import { logger } from "../utils/logger";
+import express, { Request, Response } from 'express';
+import * as ShopifyService from '../services/shopify.service';
+import { Product } from '../models/Product';
+import { logger } from '../utils/logger';
+import { getAllProducts, findProduct } from '../services/products.service';
+import {
+    findProductTag,
+    getProductTags,
+    getAllProductTags
+} from '../services/product-tags.service';
 
 /**
  * Router Definition
@@ -17,50 +23,62 @@ export const shopifyRouter = express.Router();
 
 // GET items/
 
-shopifyRouter.get("/getAllProducts", async (req: Request, res: Response) => {
-  try {
-    const products: Product[] = await ShopifyService.getAllProducts();
-    res.status(200).send(products);
-  } catch (e) {
-    res.status(404).send(e.message);
-  }
+shopifyRouter.get('/getAllProducts', async (req: Request, res: Response) => {
+    try {
+        // const result = await ShopifyService.getAllProducts();
+        res.status(200).send(await getAllProducts());
+    } catch (e) {
+        res.status(404).send(e.message);
+    }
 });
 
 //get product by ID:
 shopifyRouter.get(
-  "/products/:productId",
-  async (req: Request, res: Response) => {
-    try {
-      //+ parses string to number
-      const product: Product = await ShopifyService.findProduct(
-        +req.params.productId
-      );
-      res.status(200).send(product);
-    } catch (e) {
-      res.status(404).send(e.message);
+    '/products/:productId',
+    async (req: Request, res: Response) => {
+        try {
+            //+ parses string to number
+            const product: Product | undefined = await findProduct(
+                +req.params.productId
+            );
+            if (product) {
+                res.status(200).send(product);
+            } else {
+                res.status(500).send('Product not found');
+            }
+        } catch (e) {
+            res.status(404).send(e.message);
+        }
     }
-  }
 );
 
-shopifyRouter.get("/getAllTags", async (req: Request, res: Response) => {
-  try {
-    const tags: String[] = await ShopifyService.getAllTags();
-    res.status(200).send(tags);
-  } catch (e) {
-    res.status(404).send(e.message);
-  }
+shopifyRouter.get('/tags/getAllTags', async (req: Request, res: Response) => {
+    try {
+        const tags = await getAllProductTags();
+        res.status(200).send(tags);
+    } catch (e) {
+        res.status(404).send(e.message);
+    }
 });
 
 //get all tags for product
-shopifyRouter.get("/tags/:productId", async (req: Request, res: Response) => {
-  try {
-    const tags: String[] = await ShopifyService.getProductTags(
-      +req.params.productId
-    );
-    res.status(200).send(tags);
-  } catch (e) {
-    res.status(404).send(e.message);
-  }
+shopifyRouter.get('/tags/:productId', async (req: Request, res: Response) => {
+    try {
+        const productId = +req.params.productId;
+        if (productId) {
+            const productTag = await findProduct(productId);
+            if (productTag) {
+                const tags = await getProductTags(productId);
+                res.status(200).send(tags);
+            } else {
+                res.status(500).send('Product not found');
+            }
+        } else {
+            res.status(400).send('Invalid product ID');
+        }
+    } catch (e) {
+        res.status(404).send(e.message);
+    }
 });
 
 //   // POST items/
