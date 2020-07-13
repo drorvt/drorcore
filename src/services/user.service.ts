eb43767a01d4df73f8ad054e51e4b25f874a459c
Yonatan Back <yonatanback@gmail.com>
2020-07-13 20:11:01 +0300

    Prettier + tslint

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,27 +1,29 @@
-import { User } from "../models/User";
-import { Shop } from "../models/Shop";
-import {getConnection, getRepository} from "typeorm";
+import { User } from '../models/User';
+import { Shop } from '../models/Shop';
+import { getConnection, getRepository } from 'typeorm';
 const bcrypt = require('bcryptjs');
 
-export const createUser = async (user:User) => {
+export const createUser = async (user: User) => {
     user.password = bcrypt.hashSync(user.password, 10);
     await getConnection().manager.save(user);
 };
 
-export const addUserToShop = async (user:User, shop:Shop) => {
-    if (user && shop){
-        if (!user.shops){
+export const addUserToShop = async (user: User, shop: Shop) => {
+    if (user && shop) {
+        if (!user.shops) {
             user.shops = [];
         }
-        if (!shop.users){
+        if (!shop.users) {
             shop.users = [];
         }
         user.shops.push(shop);
-        await getConnection().manager.save([shop,user]);
+        await getConnection().manager.save([shop, user]);
     }
 };
 
-
-export const findUser = async (email:string) => {
-    return getRepository(User).findOne({ where: { email: email}, relations:['shops'] });
+export const findUser = async (email: string) => {
+    return getRepository(User).findOne({
+        where: { email: email },
+        relations: ['shops']
+    });
 };
