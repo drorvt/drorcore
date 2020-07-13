617e8249534df98666655030922ee235a5a59abc
Yonatan Back <yonatanback@gmail.com>
2020-07-13 20:13:24 +0300

    tslint + Prettier

--- tests/my-test.ts
+++ tests/my-test.ts
@@ -1,63 +1,68 @@
-import {ProductManager} from "../src/services/ProductManager";
-import {createUser, addUserToShop} from '../src/services/user.service';
-import { User } from "../src/models/User";
-import { Shop } from "../src/models/Shop";
+import { ProductManager } from '../src/services/ProductManager';
+import { createUser, addUserToShop } from '../src/services/user.service';
+import { User } from '../src/models/User';
+import { Shop } from '../src/models/Shop';
+import { syncShopify } from '../src/services/shopify.service';
 
 const fs = require('fs');
 const mysql = require('mysql');
 
 console.log('start');
 
-let str:string = fs.readFileSync('ormconfig.json', {encoding:'utf8', flag:'r'});
-let ormconfig:any = JSON.parse(fs.readFileSync('ormconfig.json', {encoding:'utf8', flag:'r'}));
-let dbName = 'pdqtest';
+const str: string = fs.readFileSync('ormconfig.json', {
+    encoding: 'utf8',
+    flag: 'r'
+});
+const ormconfig: any = JSON.parse(
+    fs.readFileSync('ormconfig.json', { encoding: 'utf8', flag: 'r' })
+);
+const dbName = 'pdqtest';
 const pool = mysql.createPool({
-    connectionLimit : 5,
-    host     : ormconfig.host,
-    user     : ormconfig.username,
-    password : ormconfig.password,
+    connectionLimit: 5,
+    host: ormconfig.host,
+    user: ormconfig.username,
+    password: ormconfig.password,
     insecureAuth: true
 });
 
-const executeQuery = async (sql:string) => {
-    return new Promise(function(resolve, reject) {            
-          pool.getConnection(function(err:any, connection:any){
-              if (err){
-                  return reject(err);
-              }
-              connection.query(sql, function(err:any, rows:any, fields:any) {    
-                  if (!err){
-                      connection.release();
-                      return resolve(rows);
-                  }
-                  else{
-                      connection.release();                        
-                      return resolve(err); 
-                  }        
-              });
-          });
-      })
-  };
+const executeQuery = async (sql: string) => {
+    return new Promise(function (resolve, reject) {
+        pool.getConnection(function (err: any, connection: any) {
+            if (err) {
+                return reject(err);
+            }
+            connection.query(sql, function (err: any, rows: any, fields: any) {
+                if (!err) {
+                    connection.release();
+                    return resolve(rows);
+                } else {
+                    connection.release();
+                    return resolve(err);
+                }
+            });
+        });
+    });
+};
 
 export const initDB = async () => {
-    await executeQuery('drop database ' + dbName).catch((err) => {
-        return Promise.resolve();   // This could fail if DB doesn't exist
+    await executeQuery('drop database ' + dbName).catch(err => {
+        return Promise.resolve(); // This could fail if DB doesn't exist
     });
     await executeQuery('create database ' + dbName);
-}
+};
 
 export const buildDemoDB = async () => {
-    let user:User = new User();
-    user.email = "test@test.com";
+    const user: User = new User();
+    user.email = 'test@test.com';
     user.isAdmin = true;
-    user.password = "xxx";
+    user.password = 'xxx';
     await createUser(user);
-    
-    let shop:Shop = new Shop();
+
+    const shop: Shop = new Shop();
     shop.apiKey = 'mykey';
     shop.name = 'demo shop';
     shop.secretKey = 'shhhh';
     shop.url = 'http://google.com';
-    
+
     addUserToShop(user, shop);
 };
