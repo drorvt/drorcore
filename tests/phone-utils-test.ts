cc1af658faae12a101dfa21997022c52b569b268
Yonatan Back <yonatanback@gmail.com>
2020-07-22 18:26:18 +0300

    separated tests

--- tests/phone-utils-test.ts
+++ tests/phone-utils-test.ts
@@ -4,17 +4,26 @@ import { validate } from '../src/utils/phone-utils';
 describe('Phone validator', function () {
     it('Should confirm a phone number is valid', function () {
         const validNumberMobile = '0545873646';
-        const validNumberCountryCode = '+972545873646';
-        const validNumberLandLine = '046515354';
-        const validNumberDash = '054-6515354';
-        const invalidNumberCharacter = '+14342f46515354';
-        const invalidNumberLength = '1434246515354';
-
         expect(validate(validNumberMobile)).to.be.true;
+    });
+    it('Should confirm a phone number with a country code is valid', function () {
+        const validNumberCountryCode = '+972545873646';
         expect(validate(validNumberCountryCode)).to.be.true;
-        expect(validate(validNumberDash)).to.be.true;
+    });
+    it('Should confirm a landline phone number is valid', function () {
+        const validNumberLandLine = '046515354';
         expect(validate(validNumberLandLine)).to.be.true;
+    });
+    it('Should confirm a phone number with a dash inside', function () {
+        const validNumberDash = '054-6515354';
+        expect(validate(validNumberDash)).to.be.true;
+    });
+    it('Should confirm a phone number with invalid characters is invalid', function () {
+        const invalidNumberCharacter = '+14342f46515354';
         expect(validate(invalidNumberCharacter)).to.be.false;
+    });
+    it('Should confirm a phone number that is too long is invalid', function () {
+        const invalidNumberLength = '1434246515354';
         expect(validate(invalidNumberLength)).to.be.false;
     });
 });
