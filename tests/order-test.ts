fde26b15db92b02c2f150e10a517ae8d17b6af6a
ShlomoSchwarcz <shlomo@j2ee.co.il>
2020-07-27 12:11:25 +0300

    reset DB between tests

--- tests/order-test.ts
+++ tests/order-test.ts
@@ -1,7 +1,7 @@
 import { createCarrier } from './../src/services/order.service';
 const expect = require('chai').expect;
 import { initDB, buildDemoDB } from './my-test';
-import { createConnection } from 'typeorm';
+import { createConnection, Connection } from 'typeorm';
 import { User } from '../src/models/User';
 import { createUser, findUser } from '../src/services/user.service';
 import * as orderService from '../src/services/order.service';
@@ -12,18 +12,24 @@ import { Order } from '../src/models/Order';
 import { Shop } from '../src/models/Shop';
 import { OrderItem } from '../src/models/OrderItem';
 import { Product } from '../src/models/Product';
+import { QueryParameters } from '../src/typings/QueryParameters';
 
-let shop: Shop;
+var shop: Shop;
 
 describe('Order Creations', function () {
-    before(async function () {
-        await createConnection({
+    var con:Connection|undefined;
+
+    beforeEach(async function () {
+        if (con){
+            await con.close();
+        }
+        con = await createConnection({
             type: 'sqlite',
             database: ':memory:',
             dropSchema: true,
             entities: ['../src/models/**/*.js', 'src/models/*.js'],
             synchronize: true,
-            logging: false
+            logging: true
         });
         shop = await buildDemoDB();
     });
@@ -69,5 +75,35 @@ describe('Order Creations', function () {
             order.id
         );
         expect(storedOrder?.items.length).to.equal(3);
+    });
+
+    it('Filter orders list', async () => {
+        let carrier: Carrier = new Carrier();
+        carrier.name = 'Fedex';
+        carrier = await createCarrier(carrier);
+
+        let order: Order = new Order();
+        order.shop = shop;
+        order.address = 'tel aviv';
+        order.radius = 1;
+        order.expected = new Date();
+        order = await orderService.createOrder(order);
+
+        order = new Order();
+        order.shop = shop;
+        order.address = 'Jerusalem';
+        order.radius = 2;
+        order.expected = new Date();
+        order = await orderService.createOrder(order);
+
+        order = new Order();
+        order.shop = shop;
+        order.address = 'Haifa';
+        order.radius = 2;
+        order.expected = new Date();
+        order = await orderService.createOrder(order);
+ 
+        let orders = await orderService.getOrders(new QueryParameters(), shop);
+        expect(orders?.length).to.equal(1);
     });
 });
